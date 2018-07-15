4f107002c85c02be56fa7cd6205ac8b3121505fc
Kunj <kunj@kunj.com>
2018-07-15 16:25:57 +0530

    Getting port where currency-exchange-service is running

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/ExchangeValue.java
@@ -8,6 +8,7 @@ public class ExchangeValue {
 	private String from;
 	private String to;
 	private BigDecimal conversionMultiple;
+	private int port;
 
 	// To create JPA entity
 	public ExchangeValue() {
@@ -36,6 +37,14 @@ public class ExchangeValue {
 
 	public BigDecimal getConversionMultiple() {
 		return conversionMultiple;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
 	}
 
 }
