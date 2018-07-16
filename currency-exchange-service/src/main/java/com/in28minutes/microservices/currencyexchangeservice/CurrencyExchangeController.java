aeb903fd9f0a0b411934aba728bf0fd6d972cb12
Kunj Bihari <kunjbihari1756@gmail.com>
2018-07-16 09:40:30 +0100

    Step 17: Created a JPA repository and wrote query method to fetch data
    from database

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -13,11 +13,14 @@ public class CurrencyExchangeController {
 
 	@Autowired
 	private Environment environment; // to get the env related info
+	
+	@Autowired
+	private ExchangeValueRepository repository;
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-
-		ExchangeValue exchangeValue = new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+		
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
 	}
